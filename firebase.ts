b742a897a65661cff8acfcc9a5a88faeb0921d64
vexastore <beirutshop55@gmail.com>
2026-05-06 03:25:25 +0300

    Add Firebase config and Firestore export

---
+++ firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAhrOE6l4uGbrNcc3ivbDTLyC1IBd63TV8",
+  authDomain: "vexa-store.firebaseapp.com",
+  projectId: "vexa-store",
+  storageBucket: "vexa-store.firebasestorage.app",
+  messagingSenderId: "544255167668",
+  appId: "1:544255167668:web:03eac2e352c45a64885be1"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
